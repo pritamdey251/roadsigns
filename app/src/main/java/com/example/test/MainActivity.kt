54eae258257f4e6eca4b0628e340307636e994e6
Pritam K Dey <pritam251@gmail.com>
2019-05-14 07:53:42 -0700

    Cleanup

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,14 +1,9 @@
 package com.example.test
 
-import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.TextView
-import android.widget.Toast
 import java.util.*
-
-//import com.example.test.R.id.textView
 
 class MainActivity : AppCompatActivity()
 {
--- app/src/main/java/com/example/test/SecondActivity.kt
+++ app/src/main/java/com/example/test/SecondActivity.kt
@@ -1,16 +1,9 @@
 package com.example.test
 
-import android.content.Intent
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
-import android.view.View
-import android.view.View.VISIBLE
-
 // Android extensions import statements for the two views to update
-import com.example.test.R
-import com.example.test.R.id.imageView
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.example.test.R.layout.activity_main
-import java.util.*
 
 class SecondActivity : AppCompatActivity()
 {
